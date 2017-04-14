99d4cd7f8a6f419937406cf35d489f395ff5deff
bonnichiwa <bonnie.pham@ryerson.ca>
2017-04-14 15:51:52 -0400

    able to access json and read through each company's name and logo

--- property-finder/src/app/app.component.ts
+++ property-finder/src/app/app.component.ts
@@ -11,23 +11,21 @@ export class AppComponent {
 
   properties = [];
 
-  constructor(private http: Http) { }
+  constructor(private http: Http,) { }
 
-   getList() {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    this.http.get('https://www.propertyfinder.ae/en/find-broker/ajax/search?page=1', options)
-      .map((res) => res.json())
-          .subscribe(
-              data => {
-                      this.properties = data;
-              },
-              err => console.error(err),
-              () => console.log(this.properties)
-          );
-      }
-
-    ngOnInit() {
-      this.getList();
+  getList() {
+  this.http.get('https://www.propertyfinder.ae/en/find-broker/ajax/search?page=1')
+    .map((res) => res.json())
+        .subscribe(
+            data => {
+                    this.properties = data.data;
+            },
+            err => console.error(err),
+            () => console.log(this.properties)
+        );
     }
+
+  ngOnInit() {
+    this.getList();
+  }
 }
