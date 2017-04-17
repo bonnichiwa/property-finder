1d07ecf57cb7832063f5790e46055b334703b196
bonnichiwa <bonnie.pham@ryerson.ca>
2017-04-16 20:12:43 -0400

    added slide in animations

--- property-finder/src/app/app.component.ts
+++ property-finder/src/app/app.component.ts
@@ -18,20 +18,29 @@ export class AppComponent {
 
   constructor(private http: Http, private counterService: CounterService) { }
 
-  getList() {
-  this.http.get('https://www.propertyfinder.ae/en/find-broker/ajax/search?page=1')
-    .map((res) => res.json())
-    .subscribe(
-      data => {
-              this.properties = data.data;
-      },
-      err => console.error(err),
-      () => console.log(this.properties)
-    );
-   }
-
   ngOnInit() {
     this.getList();
+  }
+
+  getList() {
+  this.http.get('https://www.propertyfinder.ae/en/find-broker/ajax/search?page=1')
+  .map((res) => res.json())
+  .subscribe(
+    data => {
+            this.properties = data.data;
+    },
+    err => console.error(err)
+   );
+  }
+
+  delayedAnimation() { 
+    let slideAnimation = document.getElementById('property-finder-big-column');
+    slideAnimation.className = '';
+  }
+
+  delayedFade() {
+    let nameFade = document.getElementById('carousel-show-name');
+    nameFade.className = '';
   }
 
   slideBack() {
@@ -41,24 +50,53 @@ export class AppComponent {
       this.counter = this.properties.length - 1;
     }
     this.counterService.updateCounter(this.counter);
+    let slideAnimation = document.getElementById('property-finder-big-column');
+    slideAnimation.className += 'animated bounceOutRight';
+    slideAnimation.className += 'animated bounceInLeft';
+    window.setTimeout(this.delayedAnimation, 500);
   }
 
   slideForward() {
+    let slideAnimation = document.getElementById('property-finder-big-column');
     if (this.counter >= this.properties.length - 1) {
       this.counter = 0;
     } else {
       this.counter += 1;
     }
     this.counterService.updateCounter(this.counter);
+    slideAnimation.className += 'animated bounceOutLeft';
+    slideAnimation.className += 'animated bounceInRight';
+    window.setTimeout(this.delayedAnimation, 500);
   }
 
   changeSlide(index) {
+    let slideAnimation = document.getElementById('property-finder-big-column');
+    if (this.counter > index) {
+      slideAnimation.className += 'animated bounceOutRight';
+      slideAnimation.className += 'animated bounceInLeft';
+      window.setTimeout(this.delayedAnimation, 500);
+    } else {
+      slideAnimation.className += 'animated bounceOutLeft';
+      slideAnimation.className += 'animated bounceInRight';
+      window.setTimeout(this.delayedAnimation, 500);
+    }
     this.counter = index;
     this.counterService.updateCounter(this.counter);
   }
 
   showName(name) {
     this.nameHover = name;
+    let nameFade = document.getElementById('carousel-show-name');
+    nameFade.className += "animated fadeIn";
+    window.setTimeout(this.delayedFade, 1000);
+
+  }
+
+  leaveName(name) {
+    this.nameHover = name;
+    let nameFade = document.getElementById('carousel-show-name');
+    nameFade.className += "animated fadeOut";
+    window.setTimeout(this.delayedFade, 1000);
   }
   
 }
